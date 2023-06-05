8dbe608eb4e06f2bd59c9e2fb6323531933adcb7
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-06-05 15:58:48 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form className='search-form'>
+        <input className='search-bar' type="text" />
+        <button className='search-button' type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
