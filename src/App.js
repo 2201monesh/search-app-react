a7cf629a8e7ecb04daabe07947dd3057b77d68da
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-06-05 18:55:34 +0530

    implementing the API search feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    apiSearch();
+  }, [])
+
+  const apiSearch = async () => {
+    // const search = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const search = await fetch('https://restcountries.com/v3.1/lang/spanish');
+    const response = await search.json();
+    console.log(response);
+  }
+
   return (
     <div className="App">
       <form className='search-form'>
